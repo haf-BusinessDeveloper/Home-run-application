66cf964e2e51a5f2415c416a7853dcef6383863e
haf-BusinessDeveloper <53689310+haf-BusinessDeveloper@users.noreply.github.com>
2024-01-02 22:16:08 +0200

    Create ServiceModel.php

---
+++ app/Models/ServiceModel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ServiceModel extends Model
+{
+    protected $table      = 'services_table';
+    protected $primaryKey = 'service_id';
+
+    protected $useAutoIncrement = true;
+
+    protected $returnType     = 'array';
+    protected $useSoftDeletes = true;
+
+    protected $allowedFields = [
+        
+        "service_title"
+
+    ];
+
+}
