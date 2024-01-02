dff6fad6da37cdd6ca2dbb10f6094f65f8268ecf
haf-BusinessDeveloper <53689310+haf-BusinessDeveloper@users.noreply.github.com>
2024-01-02 22:05:29 +0200

    Create UserModel.php

---
+++ app/Models/UserModel.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UserModel extends Model
+{
+    protected $table      = 'users_table';
+    protected $primaryKey = 'user_id';
+
+    protected $useAutoIncrement = true;
+
+    protected $returnType     = 'array';
+    protected $useSoftDeletes = true;
+
+    protected $allowedFields = [
+        
+        "user_email",
+        "password",
+        "full_name",
+        "national_id",
+        "phone_number",
+        "address",
+        "is_whats_available",
+        "user_status",
+        "user_created_at",
+        "user_created_by",
+        "user_updated_at",
+        "user_updated_by",
+        "user_deleted_at",
+
+    ];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'user_created_at';
+    protected $updatedField  = 'user_updated_at';
+    protected $deletedField  = 'user_deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+}
