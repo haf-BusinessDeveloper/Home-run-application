ae4dde50815a70918f485e55adbc216b4372b6e2
haf-BusinessDeveloper <53689310+haf-BusinessDeveloper@users.noreply.github.com>
2024-01-02 22:16:22 +0200

    Create TechnicianModel.php

---
+++ app/Models/TechnicianModel.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class TechnicianModel extends Model
+{
+    protected $table      = 'technicians_table';
+    protected $primaryKey = 'technician_id';
+
+    protected $useAutoIncrement = true;
+
+    protected $returnType     = 'array';
+    protected $useSoftDeletes = true;
+
+    protected $allowedFields = [
+        
+        "technician_email",
+        "password",
+        "full_name",
+        "service_id",
+        "technician_phone_number",
+        "technician_address",
+        "is_whats_available",
+        "technician_status",
+        "technician_created_at",
+        "technician_created_by",
+        "technician_updated_at",
+        "technician_updated_by",
+        "technician_deleted_at",
+
+
+    ];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'technician_created_at';
+    protected $updatedField  = 'technician_updated_at';
+    protected $deletedField  = 'technician_deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+}
