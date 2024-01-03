8a6358cc8966cd94ac8e4c9d771f83144c799c42
haf-BusinessDeveloper <53689310+haf-BusinessDeveloper@users.noreply.github.com>
2024-01-03 10:48:24 +0200

    Create and modify the Users Controller

--- app/Controllers/Dashboard/Users.php
+++ app/Controllers/Dashboard/Users.php
@@ -8,21 +8,51 @@ class Users extends BaseController
 {
     public function index()
     {
-        return view('dashboard/pages/home-page');
+        return view('dashboard/pages/users/home-page');
     }
     
     public function create()
     {
-        return view('dashboard/pages/home-page');
+        return view('dashboard/pages/users/create-page');
     }
+    
+    public function show($id = null)
+    {
+        if ($id === null) {
+            # code...
+            return redirect()->back();
+        }
+        return view('dashboard/pages/users/show-page');
+    }
+    
+    
+    public function update($id = null)
+    {
+        if ($id === null) {
+            # code...
+            return redirect()->back();
+        }
+        return view('dashboard/pages/users/update-page');
+    }
+    
+    
+    public function delete($id = null)
+    {
+        if ($id === null) {
+            # code...
+            return redirect()->back();
+        }
+        return view('dashboard/pages/users/show-page');
+    }
+    
     
     public function list()
     {
-        return view('dashboard/pages/home-page');
+        return view('dashboard/pages/users/list-page');
     }
     
     public function trash()
     {
-        return view('dashboard/pages/home-page');
+        return view('dashboard/pages/users/list-trash-page');
     }
 }
