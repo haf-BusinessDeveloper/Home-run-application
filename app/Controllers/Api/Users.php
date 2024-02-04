92acfc3b237f27b21efae2f9cafe0fca02537659
haf-BusinessDeveloper <53689310+haf-BusinessDeveloper@users.noreply.github.com>
2024-02-04 19:26:56 +0200

    ..

--- app/Controllers/Api/Users.php
+++ app/Controllers/Api/Users.php
@@ -21,8 +21,47 @@ class Users extends BaseController
         return $this->respond($data, 200);
     }
 
+    public function getUserProfile($id = null)
+    {
+        $UserModel = new UserModel();
+        $data = $UserModel->find($id);
+        unset($data['password']);
+        unset($data['is_admin']);
+        // Generic response method
+        return $this->respond($data, 200);
+    }
+
+    public function signup()
+    {
+        $postedData = $this->request->getPost();
+        if ($postedData) {
+            $UserModel = new UserModel();
+            $user_id = $UserModel->insert($postedData);
+            if ($user_id) {
+                $data = ["status" => true, "user_id" => $user_id];
+            } else {
+                $data = ["status" => false];
+            }
+            return $this->respond($data, 200);
+        }
+    }
+
     public function signin()
     {
-        
+        $postedData = $this->request->getPost();
+        if ($postedData) {
+            $UserModel = new UserModel();
+            $user_email = $postedData['user_email'];
+            $password = $postedData['password'];
+            $user_data = $UserModel->where(["user_email" => $user_email , "password" => $password ])->find();
+            if ($user_data) {
+                $data = ["status" => true, "user_id" => $user_data];
+            } else {
+                $data = ["status" => false];
+            }
+            return $this->respond($data, 200);
+        }
     }
+
+
 }
